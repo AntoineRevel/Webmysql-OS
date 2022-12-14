6ada07db3c61c5a35d84d464b2ea818aa0169a13
Antoine Revel <antoine.revel@gmail.com>
2022-12-14 17:23:22 +0100

    show list

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,22 @@
 import Head from 'next/head'
 import Price from '../components/Price'
 import prisma from '../lib/prisma'
+const { PrismaClient } = require('@prisma/client')
 
-const load = async () => {
-  try {
-    const price = await prisma.priceETH.findMany({
+export async function getStaticProps() {
+  const posts = await prisma.priceETH.findMany()
+  console.log({posts})
 
-    })
-
-
-  } catch (e) {
-    console.error(e)
-    process.exit(1)
-  } finally {
-    await prisma.$disconnect()
-  }
-  return price
-}
-
-const price =load()
-
-
-export default function Home({ price }) {
-  return (
-    <div>
-      <Head>
-        <title>PlanetScale Next.js Quickstart</title>
-        <meta name="description" content="PlanetScale Quickstart for Next.js" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="p-10 mx-auto max-w-4xl">
-        <h1 className="text-6xl font-bold mb-4 text-center">Next.js Starter</h1>
-        <p className="mb-20 text-xl text-center">
-          🔥 Shop from the hottest items in the world 🔥
-        </p>
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center  gap-4">
-        </div>
-      </main>
-
-      <footer></footer>
-    </div>
-  )
-}
-
-
+ return {
+   props : {posts: JSON.parse(JSON.stringify(posts)) }
+	  }
+	}
+  console.log('{post}');
+  export default ({posts}) =>
+<ul> 
+  bonjour
+  {posts.map(post => (
+  <li key={post.Time}>{post.Time}{post.ETHBTC}{post.ETHUSD}</li>
+   ))}
+ </ul>
+	;
